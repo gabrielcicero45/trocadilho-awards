81813dc3a773f75190769438319cbfb1b3fe03f6
gabrielcicero45@gmail.com <gabrielcicero45@gmail.com>
2023-02-14 18:07:48 -0300

    refactor: Separating data entry from the App

--- src/App.js
+++ src/App.js
@@ -1,33 +1,12 @@
-import './App.css';
-import TrocadilhoList from './components/TrocadilhoList/TrocadilhoList';
+import "./App.css";
+import PunsList from "./components/PunsList/PunsList";
+import { puns } from "./entries/puns";
 
 function App() {
-  const trocadilhos =[{date: "13/02/2023",
-  votes: 10,
-  dev: "cicero.medeirios",
-  context: "Lorem ipsum dolor,sit alit. Fugiat animi quaerat assumenda architecto possimus qui itaque reprehenderit tempore provident debitis? Dicta blanditiis mollitia reiciendis saepe. Alias, facere? Excepturi, officia obcaecati.",
-  message: "Lorem ipsus"
-  },{date: "13/02/2023",
-  votes: 10,
-  dev: "cicero.medeirios",
-  context: "Lorem ipsum dolor,sit alit. Fugiat animi quaerat assumenda architecto possimus qui itaque reprehenderit tempore provident debitis? Dicta blanditiis mollitia reiciendis saepe. Alias, facere? Excepturi, officia obcaecati.",
-  message: "Lorem ipsus"
-  },{date: "13/02/2023",
-  votes: 10,
-  dev: "cicero.medeirios",
-  context: "Lorem ipsum dolor,sit alit. Fugiat animi quaerat assumenda architecto possimus qui itaque reprehenderit tempore provident debitis? Dicta blanditiis mollitia reiciendis saepe. Alias, facere? Excepturi, officia obcaecati.",
-  message: "Lorem ipsus"
-  },{date: "13/02/2023",
-  votes: 10,
-  dev: "cicero.medeirios",
-  context: "Lorem ipsum dolor,sit alit. Fugiat animi quaerat assumenda architecto possimus qui itaque reprehenderit tempore provident debitis? Dicta blanditiis mollitia reiciendis saepe. Alias, facere? Excepturi, officia obcaecati.",
-  message: "Lorem ipsus"
-  },] 
-
   return (
     <div className="App">
       <h1>Trocadilho Awards</h1>
-      <TrocadilhoList trocadilhos={trocadilhos}/>
+      <PunsList puns={puns} />
     </div>
   );
 }
